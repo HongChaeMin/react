c8f379a7e76f71874c457a9546466a76f4e62d34
홍채민 <coals0329@cocen.com>
2022-04-22 15:54:17 +0900

    feat(part2): setting project duplication js file

---
+++ react-memo/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <ul>
+        <li>ASP.NET core</li>
+        <li>Bootstrap</li>
+        <li>react</li>
+      </ul>
+      <ul>
+        <li>Error</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
---
+++ react-memo/src/components/Books/SeeSharpGuide.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SeeSharpGuide = () => {
+    return (
+      <div>
+        <h1>강좌 리스트</h1>
+        <ul>
+          <li>리액트 소개</li>
+          <li>리액트 개발환경구축</li>
+          <li>리액트 Hello World!</li>
+        </ul>
+      </div>
+    );
+  }
+
+  export default SeeSharpGuide;
