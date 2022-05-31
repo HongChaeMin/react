02aee0ea09614c625e146feb9c64b3587677a1a6
홍채민 <coals0329@cocen.com>
2022-05-31 10:11:54 +0900

    feat(introduction): jsx

--- react/begin-react/src/App.js
+++ react/begin-react/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-import Hello from "./Hello";
+import Hello from "./Jsx";
 
 function App() {
+  // 꼭 감싸져야하는 태그
+  //두가 이상의 태그는 무조건 하나의 태그로 감싸져있어야한다
   return (
-    <div>
+    // Fragment : 단순히 감싸기 용으로 태그를 이용해야 할 때 사용
+    <>
       <Hello />
       <Hello />
       <Hello />
-    </div>
+    </>
   );
 }
 
---
+++ react/begin-react/src/Jsx.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+function Hello() {
+  // JSX 는 리액트에서 생김새를 정의할 때, 사용하는 문법
+  // html 처럼 생겼지만 Javascript 임
+  return <div className='text-green'>안녕하세요</div>
+  // class 속성은 className 으로
+}
+
+export default Hello;
